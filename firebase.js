3d136bd60b5b9ace34ad0b868ad7a9a9d9372a62
Millat-coder <mushfiqurmillat2k26@gmail.com>
2026-07-23 02:28:10 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDJDPGteJG_WkMISuv9s8649r7a5ND8sV0",
+  authDomain: "teacher-pay.firebaseapp.com",
+  projectId: "teacher-pay",
+  storageBucket: "teacher-pay.firebasestorage.app",
+  messagingSenderId: "990950893185",
+  appId: "1:990950893185:web:a90540e2fc06c73b110f80"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
