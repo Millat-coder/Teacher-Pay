8b888a305a301c7cd0d060eb771f8ae2fdef8069
Millat-coder <mushfiqurmillat2k26@gmail.com>
2026-07-23 01:49:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Teacher Pay Started");
+function addTeacher() {
+    const name = document.getElementById("teacherName").value;
+    const subject = document.getElementById("teacherSubject").value;
+    const fee = document.getElementById("teacherFee").value;
+
+    if (name === "" || subject === "" || fee === "") {
+        alert("সব তথ্য পূরণ করুন!");
+        return;
+    }
+
+    alert(`${name} সফলভাবে যোগ হয়েছে!`);
+
+    document.getElementById("teacherName").value = "";
+    document.getElementById("teacherSubject").value = "";
+    document.getElementById("teacherFee").value = "";
+}
